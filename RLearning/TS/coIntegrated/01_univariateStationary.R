5691424ffc96e63b080fa23978fa9fee7f9a1740
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-03 15:51:27 -0700

    init MA(q) section

--- RLearning/TS/coIntegrated/01_univariateStationary.R
+++ RLearning/TS/coIntegrated/01_univariateStationary.R
@@ -1,12 +1,19 @@
+#---------#---------#---------#---------#---------#---------#---------#---------
 rm(list=ls())
 
 op <- par(no.readonly=T)
-# 2 AR(p) TS Process
+
+
+# 2. AR(p) TS Process
 # Simulation of AR(1) process with φ = 0.9
 N <- 100
 PHI <- 0.9
-y <- arima.sim(n=N, list(ar=PHI), innov=rnorm(N))
 layout(matrix(c(1, 1, 2, 3), 2, 2, byrow=T))
+y <- arima.sim(n=N, list(ar=PHI), innov=rnorm(N))
 plot.ts(y, ylab='')
 acf(y, main='Autocorrelations', ylab='', ylim=c(-1, 1), ci.col=2)
 pacf(y, main='Partial Autocorrelations', ylab='', ylim=c(-1, 1), ci.col=2)
+
+
+
+# 3. MA(q) Time Series Process
