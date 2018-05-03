eef1df2d29cb928091d1ff6f9c723a70a0cda98c
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-02 18:46:39 -0700

    init 3

---
+++ Rsoft/3/textProcessingRegex.R
@@ -0,0 +1,14 @@
+#---------#---------#---------#---------#---------#---------#---------#---------
+rm(list=ls())
+
+paste('Square', 'Circle', 'Triangle', sep=':')
+shapes <- c('Square', 'Circle', 'Triangle')
+paste('My favorite shape is a', shapes)
+two.cities <- c('best', 'worst')
+paste('It was the', two.cities, 'of times.')
+paste(shapes)
+paste(shapes, collapse=' ')
+nchar('Satterthwaite')
+cases <- c('UPPER', 'lower', 'Title')
+toupper(cases)
+tolower(cases)
