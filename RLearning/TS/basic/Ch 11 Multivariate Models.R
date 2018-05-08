c4ffcfd904557efca42b81a815bbfd0b3ecab74e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-08 00:12:58 -0400

    init 11

---
+++ RLearning/TS/basic/Ch 11 Multivariate Models.R
@@ -0,0 +1,37 @@
+# 2. Spurious Regression
+setwd('~/Learning/R/RLearning/TS/basic')
+
+CBE <- read.table('./data/cbe.dat', header=T)
+elec.ts <- ts(CBE[, 3], start=1958, freq=12)
+choc.ts <- ts(CBE[, 1], start=1958, freq=12)
+plot(as.vector(aggregate(choc.ts)), as.vector(aggregate(elec.ts)))
+cor(aggregate(choc.ts), aggregate(elec.ts))
+
+x <- rnorm(100)
+y <- rnorm(100)
+for (i in 2:100) {
+  x[i] <- x[i - 1] + rnorm(1)
+  y[i] <- y[i - 1] + rnorm(1)
+}
+
+par(mfrow=c(2, 1))
+plot(x, type='l', ylim=range(c(x, y)))
+lines(y, col=2)
+
+plot(x, y)
+abline(lm(y ~ x), col=2)
+legend(
+  'topleft', legend=paste('Corr:', round(cor(x, y), 3)), bg=rgb(1, 1, 1, 0.5))
+  
+x.rates <- read.table('./data/us_rates.dat', header=T)
+head(x.rates)
+
+acf(diff(x.rates$UK))
+acf(diff(x.rates$EU))
+plot(x.rates$EU / max(x.rates$EU), type='l')
+lines(x.rates$UK / max(x.rates$UK), col=2)
+plot(x.rates$UK, x.rates$EU)
+
+
+
+# 3. Tests for Unit Root
