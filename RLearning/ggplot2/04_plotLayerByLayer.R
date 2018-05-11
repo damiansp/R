8e246c03cccb6d9ba7f9146302e289304c864447
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-11 13:39:08 -0700

    updates 4

--- RLearning/ggplot2/04_plotLayerByLayer.R
+++ RLearning/ggplot2/04_plotLayerByLayer.R
@@ -2,3 +2,11 @@ rm(list=ls())
 setwd('~/Learning/R/RLearning/ggplot2/')
 
 library(ggplot2)
+data(diamonds)
+
+p <- ggplot(diamonds, aes(x=carat))
+p <- p + layer(geom='bar', 
+               geom_params=list(fill='steelblue'), 
+               stat='bin', 
+               stat_params=list(binwidth=2))
+print(p)
