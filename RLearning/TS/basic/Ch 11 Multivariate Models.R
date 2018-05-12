a2bda245a14f15d887139bf3bb6b93c2ca8271dc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-12 18:05:00 -0400

    adds ads and pp test examples

--- RLearning/TS/basic/Ch 11 Multivariate Models.R
+++ RLearning/TS/basic/Ch 11 Multivariate Models.R
@@ -1,5 +1,9 @@
 # 2. Spurious Regression
+rm(list=rm())
 setwd('~/Learning/R/RLearning/TS/basic')
+
+library(tseries)
+
 
 CBE <- read.table('./data/cbe.dat', header=T)
 elec.ts <- ts(CBE[, 3], start=1958, freq=12)
@@ -35,3 +39,17 @@ plot(x.rates$UK, x.rates$EU)
 
 
 # 3. Tests for Unit Root
+# Augmented Dickey-Fuller test 
+# Test with null hypothesis that a = 1 for 
+# x[t] = ax[t - 1] + e[t]; where e is white noise; i.e. tests for unit root
+adf.test(x)
+# p = 0.5983: cannot reject hypothesis of a = 1
+
+# Phillips-Perron Test: Same as adf, but estimates autocorrelations in e directly
+pp.test(x.rates$UK) # p = 0.521
+pp.test(x.rates$EU) # p = 0.7297
+# Neither sig diff from unit root equation; e.g. ~ random walks
+
+
+
+# 4 Cointegration
