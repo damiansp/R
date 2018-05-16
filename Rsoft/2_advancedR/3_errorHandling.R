4539e6d0b37e6b0f6a53931d65ad600d8fe41f03
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-16 08:20:02 -0700

    fins 3

--- Rsoft/2_advancedR/3_errorHandling.R
+++ Rsoft/2_advancedR/3_errorHandling.R
@@ -2,6 +2,8 @@
 rm(list=ls())
 setwd('~/Learning/R/Rsoft/2_advancedR')
 
+library(microbenchmark)
+
 'hello' + 'world'
 as.numeric(c('5', '6', 'seven'))
 
@@ -39,3 +41,31 @@ make.NA('sodium')
 
 message('in a bottle (yeah)')
 
+
+
+# Handling Errors
+catcher <- function(expr) {
+  tryCatch(expr, 
+           error=function(e) { message('An error occurred:\n', e) },
+           warning=function(w) { warning('This is only a warning:\n', w) },
+           finally=message('Finally done!'))
+}
+
+catcher(2 + 2)
+catcher('two' + 2)
+catcher(as.numeric(c(1, 'two', 3)))
+
+is.even <- function(n) {
+  tryCatch(n %% 2 == 0, error=function(e) { F })
+}
+
+is.even(714)
+is.even('eight')
+
+# But faster to do
+is.even2 <- function(n) {
+  is.numeric(n) && n %% 2 == 0
+}
+
+microbenchmark(sapply(letters, is.even))
+microbenchmark(sapply(letters, is.even2))
