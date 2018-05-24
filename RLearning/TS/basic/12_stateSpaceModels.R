cd5819d7cd200c110cb67cd45ebacee88c7885bb
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-24 16:01:32 -0700

    init 12

---
+++ RLearning/TS/basic/12_stateSpaceModels.R
@@ -0,0 +1,12 @@
+#---------#---------#---------#---------#---------#---------#---------#---------
+rm(list=ls())
+setwd('~/Learning/R/RLearning/TS/basic')
+
+
+# 1. Purpose
+# Focus on applications with parameers that adapt over time
+
+
+
+# 2. Linear State Space Models
+# 2.1 Dynamic linear models
