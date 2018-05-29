f9f8f3b7560771b101906d2624bef4fb298b5fe5
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-29 10:16:10 -0700

    adds pivot table to LongitudinalData

--- Rsoft/4_OOP/oop_code.R
+++ Rsoft/4_OOP/oop_code.R
@@ -2,30 +2,15 @@
 rm(list=ls())
 setwd('~/Learning/R/Rsoft/4_OOP')
 
-#library(dplyr)
+library(dplyr)
+library(readr)
 library(magrittr)
+library(reshape)
 library(tidyr)
-
-#LongitudinalData <- setRefClass(
-#  'LongitudinalData',
-#  fields=list(df='data.frame'),
-#  methods=list(
-#    show = function() {
-#      cat(sprintf('Longitudinal data set with %d subjects', 
-#                  length(unique(df$id)))),
-#    },
-#    subject = function() {
-#      
-#    })
-#)
-
-#make_LD <- function(df) {
-#  LongitudinalData$new(df=df)
-#}
 
 setClass('Subject',
          slots=list(id='numeric',
-                    data='matrix'))
+                    data='data.frame'))
 setGeneric(
   'summary',
   function(x) {
@@ -35,12 +20,15 @@ setMethod(
   'summary',
   c(x='Subject'),
   function(x) {
-  	print(sprintf('ID: %d', x@id))
-  	print(head(x$dataframe))
+    print.summary <- function() {
+      cat(sprintf('ID: %d\n%s', x@id, capture.output(x@data)))	
+    }
+    capture.output(print.summary())
   })
 
 setClass('LongitudinalData',
-         slots=list(dataframe='data.frame'))
+         slots=list(dataframe='data.frame',
+                    pivot='data.frame'))
          
          
 setGeneric('print')
@@ -55,66 +43,74 @@ setMethod(
   'print',
   c(x='Subject'),
   function(x) {
-  	cat(sprintf('ID: %d\n', x@id))
-  	print(x@data)
+  	cat(sprintf('Subject ID: %d\n', x@id))
   })
   
 setGeneric(
   'subject',
-  function(x, id) {
+  function(x, idn) {
   	standardGeneric('subject')
   })
 setMethod(
   'subject',
-  c(x='LongitudinalData'),
-  function(x, id) {
-    sub <- subset(x@dataframe,  id == id) 
-    visit <- sort(unique(sub$visit))
-    temp <- list()
-    for (v in visit) {
-      ssub <- subset(sub, visit == v)
-      temp[[v + 1]] <- tapply(ssub$value, ssub$room, mean)
-    }
-    m <- temp[[1]]
-    for (i in 2:length(temp)) {
-      m <- rbind(m, temp[[i]])
-    }
-    m <- cbind(visit, m)
-    rownames(m) <- rep('', length(visit))
-	new('Subject', id=id, data=m)
+  c(x='LongitudinalData', idn='numeric'),
+  function(x, idn) {
+  	if (!idn %in% x@pivot$id) {
+  	 return (NULL)
+  	}
+  	subject.table <- subset(x@pivot, id == idn)
+  	# Keep columns only if at least one NA value
+    keep <- apply(test, 
+                  2, 
+                  function(x) ifelse(length(x) == sum(is.na(x)), F, T))
+    data <- subject.table[, keep]
+    new('Subject', id=idn, data=data[, -which(names(data) == 'id')])
   })
+  	
          
 make_LD <- function(df) {
-  new('LongitudinalData', dataframe=df)
+  pivot <- data %>%
+    group_by(id, visit, room) %>%
+    summarize(mean.value = mean(value)) %>%
+    spread(room, mean.value) %>%
+    as.data.frame()
+  new('LongitudinalData', dataframe=df, pivot=pivot)
 }
 
 
 
-data <- read.csv('../data/MIE.csv')
+# Test code------------------------------------------------------------
+data <- read_csv("../data/MIE.csv")
 head(data)
 x <- make_LD(data)
-class(x)
+print(class(x))
 print(x)
+
+head(x@dataframe)
+head(x@pivot)
+
+test <- subset(x@pivot, id == 106)
+test
+
+## Subject 10 doesn't exist
+out <- subject(x, 10)
+print(out)
+
 out <- subject(x, 14)
 print(out)
-s <- subset(data, id==14 & visit==0)
-head(s)
-mean(s$value)
 
-    sub <- subset(data,  id == id) 
-    head(sub)
-    visit <- sort(unique(sub$visit))
-    temp <- list()
-    for (v in visit) {
-      ssub <- subset(sub, visit == v)
-      head(ssub)
-      temp[[v + 1]] <- tapply(ssub$value, ssub$room, mean)
-    }
-    m <- temp[[1]]
-    for (i in 2:length(temp)) {
-      m <- rbind(m, temp[[i]])
-    }
-    m <- cbind(visit, m)
-    rownames(m) <- rep('', length(visit))
+out <- subject(x, 54) %>% summary
+print(out)
 
-mean(subset(data, id == 14 & visit == 0)$value)
+out <- subject(x, 14) %>% summary
+print(out)
+
+out <- subject(x, 44) %>% visit(0) %>% room("bedroom")
+print(out)
+
+## Show a summary of the pollutant values
+out <- subject(x, 44) %>% visit(0) %>% room("bedroom") %>% summary
+print(out)
+
+out <- subject(x, 44) %>% visit(1) %>% room("living room") %>% summary
+print(out)
