5c05073080dc9d046bcc3ee2f66eb3d7f7c93957
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-02 16:15:06 -0400

    reformatted up to 'Unusual points'

--- RLearning/ExtendingLM/Ch06GeneralizedLinearModels.R
+++ RLearning/ExtendingLM/Ch06GeneralizedLinearModels.R
@@ -4,9 +4,7 @@
 #		Generalized Linear, Mixed Effects and NonParametric Regression 	#
 #		Models		Faraway (2006)										#
 #																		#
-#=======================================================================#
-
-#===================================#
+#===================================#===================================#
 #									#
 #	6. Generalized Linear Models	#
 #									#
@@ -16,7 +14,7 @@ library(faraway)
 setwd('~/Learning/R/RLearning/ExtendingLM')
 
 
-# 6.2 Fitting a GLM
+# 2 Fitting a GLM
 data(bliss)
 head(bliss)
 modl <- glm(cbind(dead, alive) ~ conc, family=binomial, bliss)
@@ -60,7 +58,7 @@ summary(lmod)$coef[, 2] / summary(lmod)$sigma
 
 
 
-# 6.3 Hypothesis Tests
+# 3 Hypothesis Tests
 summary(modl)
 
 # test goodness of fit by comparing res.dev to res. df:
@@ -76,79 +74,99 @@ anova(modl2, test="Chi")	# quadratic term not signif; reduces to previous
 
 
 
-# 6.4 GLM Diagnostics
+# 4 GLM Diagnostics
+# Residuals
 resid(modl)	# Deviance residuals
 resid(modl, "pearson")
-resid(modl, "response")
-resid(modl, 'working')
-
+resid(modl, "response") # =
+bliss$dead / n - fitted(modl)
+resid(modl, 'working') # = 
+modl$residuals         # =
 resid(lmod)
-
 
 # Leverage and Influence
 influence(modl)$hat	# leverages -- all roughly equal - good
 rstudent(modl)	# studentized resids
 # change in fit due to omitting a case can be done by comparing coefs:
-influence(modl)$coef	#in this case datum 1 has largest influence
+influence(modl)$coef # in this case datum 1 has largest influence
 cooks.distance(modl)
-
 
 # Model Diagnostics
 # Give the typically non-normal distribution of residuals, it is generally 
 # more informative to plot the linear predictors (eta.hat) than the predicted
 # response (mu.hat) as with lms.
+# Using mu.hat
 data(gala)
-gala = gala[,-2]; head(gala)
-modp = glm(Species ~., family = poisson, gala)
-plot(resid(modp) ~ predict(modp, type = "response"), 
-	 xlab = expression(hat(mu)), ylab = "Deviance residuals")
-plot(resid(modp) ~ predict(modp, type = "link"), xlab = expression(hat(eta)),
- 	 ylab = "Deviance residuals")
-# Note, this (et.hat) is the first plot given for plot(model.glm):
-par(mfrow = c(2, 2))
+gala <- gala[, -2]
+head(gala)
+modp <- glm(Species ~ ., family=poisson, gala)
+plot(resid(modp) ~ predict(modp, type="response"), 
+	 xlab=expression(hat(mu)), 
+	 ylab="Deviance residuals")
+plot(resid(modp) ~ predict(modp, type="link"), 
+     xlab=expression(hat(eta)),
+ 	 ylab="Deviance residuals")
+# Note, this (eta.hat) is the first plot given for plot(model.glm):
+par(mfrow=c(2, 2))
 plot(modp)
 
 # By plotting the response, we can unscale the variance to view the (known) 
 # non-constant nature
-plot(resid(modp, type = "response") ~ predict(modp, type = "link"), 
-	 xlab = expression(hat(eta)), ylab = "Response residuals")
-# The response variable is count (Poisson-distributed), so this is expected, 
-# and why we want to use a glm in the first place
+par(mfrow=c(1, 1))
+plot(resid(modp, type="response") ~ predict(modp, type="link"), 
+	 xlab=expression(hat(eta)), 
+	 ylab="Response residuals")
+# The response variable is count (Poisson-distributed), so the non-constant 
+# variance is expected, and why we want to use a glm in the first place
 
-
+# Examine relationship between predictors and response
 plot(Species ~ Area, gala)
 plot(Species ~ log(Area), gala)
-mu = predict(modp, type = "response")
+mu <- predict(modp, type="response")
 # linearize the response to meet glm assumptions
 # z = eta         + (y            - mu) / mu == eta + (y - mu) * (deta/dmu)
-z = predict(modp) + (gala$Species - mu) / mu
-plot(z ~ log(Area), gala, ylab = "Linearized response")
+z <- predict(modp) + (gala$Species - mu)/mu
+plot(z ~ log(Area), gala, ylab="Linearized response")
 
 # In this model, log transformations of all variables help to linearize
-modpl = glm(Species ~ log(Area) + log(Elevation) + log(Nearest) + 
-			log(Scruz + 0.1) + log(Adjacent), family = poisson, gala)
+modpl <- glm(Species ~ log(Area) + log(Elevation) + log(Nearest) + 
+			   log(Scruz + 0.1) + log(Adjacent), 
+			 family=poisson, 
+			 gala)
 c(deviance(modp), deviance(modpl)) # substantial reduction in deviance
 
 # Linearizing variables with respect to the response only may be problematic
-# though, as it fails to account for the effects of the other variable.
+# though, as it fails to account for the effects of the other variables.
 # For lms, partial resid plots are used for this purpose; in glms they can be
 # applied as: z - eta.hat + beta[j]x[j] ~ x[j]
 # For example, the log(Area) predictor:
-mu = predict(modpl, type = "response")
+mu <- predict(modpl, type="response")
 #u = z - eta.hat             + beta[Area]    *x
-u = (gala$Species - mu) / mu + coef(modpl)[2]*log(gala$Area)
-plot(u ~ log(Area), gala, ylab = "Partial Residual")
-abline(0, coef(modpl)[2], col = 2) # No apparent reason for concern
+u <- (gala$Species - mu)/mu + coef(modpl)[2]*log(gala$Area)
+plot(u ~ log(Area), gala, ylab="Partial Residual", xlab="log(Area)")
+abline(0, coef(modpl)[2], col=2) # No apparent reason for concern
 
 # The same can be achieved with:
-residuals(modpl, type = "partial")	# Partial residuals by predictor
-plot(residuals(modpl, type = "partial")[,1] ~ log(gala$Area)) 
+residuals(modpl, type="partial")	# Partial residuals by predictor
+par(mfrow=c(2, 3))
+plot(residuals(modpl, type="partial")[, 1] ~ log(gala$Area)) 
+abline(0, coef(modpl)[2], col=2)
+plot(residuals(modpl, type="partial")[, 2] ~ log(gala$Elevation)) 
+abline(0, coef(modpl)[3], col=2)
+plot(residuals(modpl, type="partial")[, 3] ~ log(gala$Nearest)) 
+abline(0, coef(modpl)[4], col=2)
+plot(residuals(modpl, type="partial")[, 4] ~ log(gala$Scruz + 0.1)) 
+abline(0, coef(modpl)[5], col=2)
+plot(residuals(modpl, type="partial")[, 5] ~ log(gala$Adjacent)) 
+abline(0, coef(modpl)[6], col=2)
 
 # Check assumption of validitiy of the particular link function: (plot z 
 # against eta)
-z = predict(modpl) + (gala$Species - mu) / mu
-plot(z ~ predict(modpl), xlab = "Linear predictor", 
-	 ylab = "Linearized response")
+z <- predict(modpl) + (gala$Species - mu)/mu
+par(mfrow=c(1, 1))
+plot(z ~ predict(modpl), 
+     xlab="Linear predictor (z)", 
+	 ylab="Linearized response (eta)")
 
 # Unusual Points
 # Look for outliers in the fitted residuals:
