2af03c0c8b046bb83f0f19fea99caad701d5d5a8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-05 00:10:48 -0400

    adds gehar test

--- RLearning/NonparametricStats/06TimeToEventAnalysis.R
+++ RLearning/NonparametricStats/06TimeToEventAnalysis.R
@@ -5,6 +5,7 @@ setwd('~/Learning/R/RLearning/NonparametricStats')
 library(npsm)
 library(Rfit)
 library(survival)
+data(cancertrt)
 data(hemorrhage)
 
 # 2 Kaplan-Meier and Log Rank Test
@@ -23,3 +24,16 @@ legend('bottomleft', legend=c('Group 1', 'Group 2'), lty=1, col=1:2, bty='n')
 survdiff(Surv(hemorrhage$time, hemorrhage$recur) ~ hemorrhage$genotype)
 # On avg. genotype = 0 expected to live 9.28 - 8.72 (= 0.56) months longer 
 # (p = 0.01)
+
+
+# 2.1 Gehan's test
+head(cancertrt)
+args(gehan.test)
+gehan.test(cancertrt$time, cancertrt$event, cancertrt$trt)
+
+
+
+# 3. Cox Proportional Hazard Models
+fit <- coxph(Surv(time, recur) ~ genotype, data=hemorrhage)
+summary(fit3) # The coef, 1.33 indicates increased risk for genotype of group 2
+# Alternately est. risk of hemorrhage for hetero is 3.878 over homo 
