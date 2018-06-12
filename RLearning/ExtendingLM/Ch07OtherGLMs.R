82012c2d2ebadf79ffd10a6f12bc2d0a38817428
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-12 08:53:17 -0700

    updates to 7.3

--- RLearning/ExtendingLM/Ch07OtherGLMs.R
+++ RLearning/ExtendingLM/Ch07OtherGLMs.R
@@ -14,9 +14,11 @@ setwd('~/Learning/R/RLearning/ExtendingLM/')
 
 library(faraway)
 library(MASS)
-#library(SuppDists)
+library(statmod)
+data(cpd)
 data(motorins)
 data(wafer)
+data(weldstrength)
 
 # 1 Gamma GLM
 # Used for continuous, skewed responses
@@ -91,49 +93,57 @@ c(exp(10.998), exp(10.998)*0.16145)
 
 
 
-# 7.2 Inverse Gaussian GLM
-x = seq(0, 8, 0.01)
-r = 1
-b = 0
-color = rgb(r, 0, b) 
-plot(x, dinvGauss(x, 1, 0.5), xlab='', ylab='', ylim=c(0, 1.5), xaxs='i', 
-	 yaxs='i', type='l', col = color)
+# 2 Inverse Gaussian GLM
+x <- seq(0, 8, 0.01)
+r <- 1
+b <- 0
+color <-rgb(r, 0, b) 
+plot(x, 
+     dinvgauss(x, 1, 0.5),
+     xlab='', 
+     ylab='', 
+     ylim=c(0, 1.5), 
+     xaxs='i', 
+	 yaxs='i', 
+	 type='l', 
+	 col=color)
 	 
-for (i in seq(1, 3, length = 10)) {
-	r = r - 0.09
-	for (j in seq(1, 5, length = 10)) {
-		b = b + 0.09
-		if (b > 1) { b = 0 }
-		lines(x, dinvGauss(x, i, j), col = rgb(r, 0, b))
-	}
+for (i in seq(1, 3, length=10)) {
+  r <- r - 0.09
+  for (j in seq(1, 5, length=10)) {
+    b <- b + 0.09
+    if (b > 1) { b=0 }
+      lines(x, dinvgauss(x, i, j), col=rgb(r, 0, b))
+  }
 }
 
-
-data(cpd)
+head(cpd)
 plot(cpd$actual ~ cpd$projected)
-lmod = lm(actual ~ projected - 1, cpd)
+lmod <- lm(actual ~ projected - 1, cpd)
 summary(lmod)
-igmod = glm(actual ~ projected - 1, 
-			family = inverse.gaussian(link='identity'), cpd)
+igmod <- glm(actual ~ projected - 1, 
+			 family=inverse.gaussian(link='identity'), 
+			 cpd)
 summary(igmod)
-plot(resid(igmod) ~ log(fitted(igmod)), ylab = 'Dev. Resid', 
+plot(resid(igmod) ~ log(fitted(igmod)), 
+     ylab='Dev. Resid', 
 	 xlab=expression(log(hat(mu))))
-abline(h = 0)
+abline(h=0)
 
 
 
-# 7.3 Joint Modeling of the Mean and Dispersion
-data(weldstrength)
-lmod = lm(Strength ~ Drying + Material + Preheating, weldstrength)
+# 3 Joint Modeling of the Mean and Dispersion
+lmod <- lm(Strength ~ Drying + Material + Preheating, weldstrength)
 summary(lmod)
 
-h = influence(lmod)$hat
-d = resid(lmod)^2 / (1 - h)
-gmod = glm(d ~ Material + Preheating, family = Gamma(link = log), 
-		   weldstrength, weights = 1 - h)
-w = 1 / fitted(gmod)
-lmod = lm(Strength ~ Drying + Material + Preheating, weldstrength, 
-		  weights = w)
+h <- influence(lmod)$hat
+d <- resid(lmod)^2 / (1 - h)
+gmod <- glm(d ~ Material + Preheating, 
+            family=Gamma(link=log), 
+		    weldstrength, 
+		    weights=1 - h)
+w <- 1 / fitted(gmod)
+lmod <- lm(Strength ~ Drying + Material + Preheating, weldstrength, weights=w)
 summary(lmod)
 summary(gmod)
 
