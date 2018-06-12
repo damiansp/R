dcad60adcf0de592814f7b60ca3f810a0b359cea
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-12 09:10:30 -0700

    cov vs cor

--- RLearning/Multivariate/03_pca.R
+++ RLearning/Multivariate/03_pca.R
@@ -4,4 +4,6 @@ setwd('~/Learning/R/RLearning/Multivariate')
 
 
 
-# 
+# 4. Should PCs be Extracted from Covariance or Correlation Matrix?
+# Really no reason you should not always use correlations -- scale invariant
+ 
