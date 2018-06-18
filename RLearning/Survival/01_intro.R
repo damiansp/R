241902aa0801ac9699a68775fba37c539fa4cd71
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-18 08:37:59 -0700

    +1

---
+++ RLearning/Survival/01_intro.R
@@ -0,0 +1,14 @@
+#---------#---------#---------#---------#---------#---------#---------#---------
+rm(list=ls())
+setwd('~/Learning/R/RLearning/Survival')
+
+library(asaur)
+
+
+
+# 4. Some examples of Survival Data Sets
+head(gastricXelox)
+head(pancreatic)
+head(prostateSurvival)
+head(pharmacoSmoking)
+head(hepatoCellular)
