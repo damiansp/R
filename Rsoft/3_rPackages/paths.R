5168b0ae5f5e537f87da9086dca4ca9e8b7c5f59
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-19 08:23:27 -0700

    +env paths rappdir writes

---
+++ Rsoft/3_rPackages/paths.R
@@ -0,0 +1,6 @@
+data.file <- file.path('~', 'Desktop', 'file.txt') # Sys-specific path generated
+Sys.info()['sysname']
+path.expand('~')
+normalizePath('.')
+normalizePath('~')
+basename(data.file)
