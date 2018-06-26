f874789a91b4fb32bd16fb46fee9787b58da4de8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-26 08:18:55 -0700

    +04; +MC2

--- RLearning/MonteCarlo/02_randomVariableGeneration.R
+++ RLearning/MonteCarlo/02_randomVariableGeneration.R
@@ -34,6 +34,8 @@ hist(X, freq=F, main='Exp from Uniform')
 lines(density(X), col=4)
 hist(Y, freq=F, main='Exp from rexp')
 lines(density(Y), col=4)
+
+
 		
 # 2 General Transformation Methods
 U <- runif(3000)
@@ -46,28 +48,28 @@ hist(X)
 
 # 2.1 A Normal Generator
 # 2.2 Discrete Distributions
-	nSim <- 4000
-	lambda <- 100
-	spread <- 3 * sqrt(lambda) # ~3sd
-	tt <- round(seq(max(0, lambda - spread), lambda + spread, 1))
-	prob <- ppois(tt, lambda)
-	plot(prob ~ tt, type='l')
-	X <- numeric(nSim)
-	for (i in 1:nSim) {
-		u <- runif(1)
-		X[i] <- tt[1] + sum (prob < u)
-	}
-	hist(X)
+n.sim <- 4000
+lambda <- 100
+spread <- 3 * sqrt(lambda) # ~3sd
+tt <- round(seq(max(0, lambda - spread), lambda + spread, 1))
+prob <- ppois(tt, lambda)
+plot(prob ~ tt, type='l')
+X <- numeric(n.sim)
+for (i in 1:n.sim) {
+  u <- runif(1)
+  X[i] <- tt[1] + sum (prob < u)
+}
+hist(X)
 		
-	# 2.2.3 Mixture Representations
-	# Ex: X ~ Neg(n, p) then: X|y ~ P(y) and Y ~ G(n, beta)
-	nSim <- 1000
-	n <- 6
-	p <- 0.3
-	y <- rgamma(nSim, n, rate=p / (1 - p))
-	x <- rpois(nSim, y)
-	hist(x, main='', freq=F, col='cyan', border='blue', breaks=40)
-	lines(1:50, dnbinom(1:50, n, p), lwd=2, col='sienna')
+# 2.3 Mixture Representations
+# Ex: X ~ Neg(n, p) then: X|y ~ P(y) and Y ~ G(n, beta)
+n.sim <- 1000
+n <- 6
+p <- 0.3
+y <- rgamma(n.sim, n, rate=p / (1 - p))
+x <- rpois(n.sim, y)
+hist(x, main='', freq=F, col='cyan', border='blue', breaks=40)
+lines(0:50, dnbinom(0:50, n, p), lwd=2, col='sienna')
 		
 
 
--- RLearning/Multivariate/04_multidimScaling.R
+++ RLearning/Multivariate/04_multidimScaling.R
@@ -2,3 +2,37 @@
 rm(list=ls())
 setwd('~/Learning/R/RLearning/Multivariate')
 
+library(MVA)
+
+
+# 4. Classical Multidimensional Scaling
+# 4.2 Examples 
+X <- matrix(c(3, 4, 4, 6, 1,
+              5, 1, 1, 7, 3,
+              6, 2, 0, 2, 6,
+              1, 1, 1, 0, 3,
+              4, 7, 3, 6, 2,
+              2, 2, 5, 1, 0,
+              0, 4, 1, 1, 1,
+              0, 6, 4, 3, 5,
+              7, 6, 5, 1, 4,
+              2, 1, 4, 3, 1),
+            nrow=10,
+            byrow=F)
+(D <- dist(X))
+cmdscale(D, k=9, eig=T) 
+# if able to fully recover dist matrix, the following should be ~0
+max(abs(dist(X) - dist(cmdscale(D, k=5))))
+# cf PCA
+max(abs(prcomp(X)$x) - abs(cmdscale(D, k=5)))
+
+# Non-Euclidean
+X.m <- cmdscale(dist(X, method='manhattan'), k=nrow(X) - 1, eig=T)
+(X.eigen <- X.m$eig)
+cumsum(abs(X.eigen)) / sum(abs(X.eigen))
+plot(cumsum(abs(X.eigen)) / sum(abs(X.eigen)), type='l')
+cumsum(X.eigen^2) / sum(X.eigen^2)
+plot(cumsum(X.eigen^2) / sum(X.eigen^2), type='l')
+# both suggest 3D solution is good
+
+airline.mds <- cmdscale(airdist, k=9, eig=T)
