e25672cbe72efda9e1a721e3b72d5776212cd436
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-10 07:45:05 -0700

    MA(q) sims

--- RLearning/TS/coursera/wk3.R
+++ RLearning/TS/coursera/wk3.R
@@ -1,1 +1,7 @@
+#=========#=========#=========#=========#=========#=========#=========#=========
 setwd('~/Learning/R/RLearning/TS/coursera')
+
+par(mfrow=c(3, 1))
+plot(arima.sim(n=150, list(order=c(0, 0, 0))), main='White noise')
+plot(arima.sim(n=150, list(ma=c(0.33, 0.33, 0.33))), main='MA(3)')
+plot(arima.sim(n=150, list(ma=rep(0.2, 5))), main='MA(5)')
