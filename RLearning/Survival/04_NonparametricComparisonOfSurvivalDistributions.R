2a1a20be3c088764430d023a58f3a6d17f35d2d1
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-11 07:55:45 -0700

    fin 04

--- RLearning/Survival/04_NonparametricComparisonOfSurvivalDistributions.R
+++ RLearning/Survival/04_NonparametricComparisonOfSurvivalDistributions.R
@@ -34,3 +34,25 @@ plot(survfit(Surv(pfs.month) ~ pancreatic$stage),
 legend(
   'topright', lty=1, col=c(4, 2), legend=c('Locally Advanced', 'Metastatic'))
 survdiff(Surv(pfs) ~ pancreatic$stage, rho=1)
+
+
+
+# 2 Stratified Test
+head(pharmacoSmoking)
+survdiff(Surv(pharmacoSmoking$ttr, pharmacoSmoking$relapse) ~ pharmacoSmoking$grp)
+
+lambda.mutant.0 <- 0.03 # hazard
+lambda.mutant.1 <- 0.03 * 0.55
+lambda.wt.0 <- 0.03 * 0.2
+lambda.wt.1 <- 0.03 * 0.2 * 0.55
+set.seed(1103)
+tt.control.mutant <- rexp(25, rate=lambda.mutant.0)
+tt.treat.mutant <- rexp(125, lambda.mutant.1)
+tt.control.wt <- rexp(125, lambda.wt.0)
+tt.treat.wt <- rexp(25, lambda.wt.1)
+tt.all <- c(tt.control.mutant, tt.treat.mutant, tt.control.wt, tt.treat.wt)
+status <- rep(1, length(tt.all))
+genotype <- c(rep('mutant', 150), rep('wt', 150))
+trt <- c(rep(0, 25), rep(1, 125), rep(0, 125), rep(1, 25))
+survdiff(Surv(tt.all, status) ~ trt)
+survdiff(Surv(tt.all, status) ~ trt + strata(genotype))
