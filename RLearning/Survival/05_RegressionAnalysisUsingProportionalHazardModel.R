0d21256d1bd1f89d456894aabb4dd0101f95e97e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-14 21:06:54 -0400

    5++

--- RLearning/Survival/05_RegressionAnalysisUsingProportionalHazardModel.R
+++ RLearning/Survival/05_RegressionAnalysisUsingProportionalHazardModel.R
@@ -1,6 +1,7 @@
 #=========#=========#=========#=========#=========#=========#=========#=========
 rm(list=ls())
 setwd('~/Learning/R/RLearning/Survival')
+library(numDeriv)
 
 
 # 2. Comparing Two Survival Distributions Using a Partial Likelihood Function
@@ -23,3 +24,26 @@ plot(p.likelihoods ~ betas, type='l')
 abline(h=result$value, col=2)
 abline(v=result$par, col=4, lty=2)
 
+
+
+# 3. Partial Likelihood Hypothesis Tests
+# 3.3 The likelihood ratio test
+head(pharmacoSmoking)
+#?coxph # proportional hazards
+result.cox <- coxph(Surv(tt, status) ~ grp) # data???
+summary(result.cox) # will give scores for Likelihood ratio test, Wald test and 
+                    # Score (logrank) test
+grad(func=partial.likelihood.simple, x=0) # slope of tangent at 1 SE, and hence 
+                                          # measure of information
+hessian(func=partial.likelihood.simple, x=0) # 2nd derivative, or curvature at 1 SE
+pchisq(1.274, df=1, lower.tail=F) 
+h <- hessian(func=partial.likelihood.simple, x=result.cox$par)
+sqrt(1 / -h) # SE
+p <- 2 * pnorm(coef / sqrt(1 / -h), lower.tail=F)
+beta.hat <- result.cox$par
+likelihood.ratio <- 2 * (partial.likelihood.simple(beta.hat) 
+                         - partial.likelihood.simple(0))
+p <- pchisq(likelihood.ratio, 1, lower.tail=F)
+
+
+                         
