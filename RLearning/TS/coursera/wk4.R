1bf7dbfe37f46b3191700cacad049af5b0f61065
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-08-24 08:46:08 -0700

    4++

--- RLearning/TS/coursera/wk4.R
+++ RLearning/TS/coursera/wk4.R
@@ -2,8 +2,13 @@
 rm(list=ls())
 setwd('~/Learning/R/RLearning/TS/coursera')
 
+library(isdals)
+library(ppcor)
+data(bodyfat)
+data(LakeHuron)
 
-# 1. PACF to Estimate the Order of AR(p) Processes
+
+# 1.1. PACF to Estimate the Order of AR(p) Processes
 phi1 <- 0.9
 phi2 <- -0.6
 data.ts <- arima.sim(n=500, list(ar=c(phi1, phi2)))
@@ -27,3 +32,23 @@ acf(na.omit(y))
 pacf(na.omit(y))
 
 ar(na.omit(y), order.max=5) # infers coefs 0.72, -0.30
+
+
+pairs(bodyfat[, c('Fat', 'Triceps', 'Thigh', 'Midarm')])
+cor(bodyfat[, c('Fat', 'Triceps', 'Thigh', 'Midarm')])
+fat.hat <- predict(lm(Fat ~ Thigh, bodyfat))
+triceps.hat <- predict(lm(Triceps ~ Thigh, bodyfat))
+cor((bodyfat$Fat - fat.hat), (bodyfat$Triceps - triceps.hat)) # partial correlation
+
+# Can be gotten by:
+pcor(bodyfat[, c('Fat', 'Triceps', 'Thigh')])
+pcor(bodyfat[, c('Fat', 'Triceps', 'Thigh', 'Midarm')])
+
+par(mfrow=c(3, 1))
+plot(LakeHuron)
+acf(LakeHuron)
+pacf(LakeHuron)
+
+
+
+# 2. Yule-Walker Equations (Matrix Notation) and Estimating AR(p) Parameters
