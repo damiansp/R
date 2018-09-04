46d54d5687b41951c2a79ef55319a2c56b9a182d
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-04 08:12:35 -0700

    02++

--- RLearning/GraphicalModels/02_LogLinearModels.R
+++ RLearning/GraphicalModels/02_LogLinearModels.R
@@ -1,8 +1,35 @@
 #=========#=========#=========#=========#=========#=========#=========#=========
+rm(list=ls())
 setwd('~/Learning/R/RLearning/GraphicalModels')
+
+library(ggm)
+library(gRbase)
+library(igraph)
+library(lcd)
+library(RBGL)
+library(Rgraphviz)
+library(sna)
+data(dumping)
+data(lizard)
+data(lizardAGG)
+data(lizardRAW)
+data(mildew)
+data(reinis)
 
 
 
 # 2. Preliminaries
 # 2.1 Four data sets
+str(reinis)
+str(mildew)
+str(dumping)
 
+
+# 2.2 Data Formats
+head(lizardRAW)
+lizardAGG
+lizard
+
+
+
+# 3. Log-Linear Models
