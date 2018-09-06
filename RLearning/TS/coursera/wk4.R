c9820075e84d8e9c0bea87570a0265d8186be19b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-06 08:37:15 -0700

    +Yule Walker ex

--- RLearning/TS/coursera/wk4.R
+++ RLearning/TS/coursera/wk4.R
@@ -52,3 +52,30 @@ pacf(LakeHuron)
 
 
 # 2. Yule-Walker Equations (Matrix Notation) and Estimating AR(p) Parameters
+sigma <- 4
+phi <- c(1/3, 1/2)
+n <- 1000
+
+# Sim AR process
+ar.process <- arima.sim(n, model=list(ar=phi), sd=sigma)
+ar.process[1:5]
+r <- acf(ar.process, plot=F)$acf[2:3]
+r
+R <- matrix(1, 2, 2)
+R
+R[1, 2] <- r[1]
+R[2, 1] <- r[1]
+b <- matrix(r, 2, 1)
+b
+phi.hat <- matrix(c(solve(R, b)[1, 1], solve(R, b)[2, 1]), 2, 1)
+phi.hat
+
+# Variance estimation
+c0 <- acf(ar.process, type='covariance', plot=F)$acf[1]
+var.hat <- c0 * (1 - sum(phi.hat * r))
+var.hat
+
+par(mfrow=c(3, 1))
+plot(ar.process, main='Simulated AR(2)')
+acf(ar.process, main='ACF')
+pacf(ar.process, main='PACF')
