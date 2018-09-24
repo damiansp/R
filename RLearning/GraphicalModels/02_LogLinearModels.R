0857c0f88350d5073f0720da0beaef14c8d08883
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-24 08:47:17 -0700

    fin sec 3

--- RLearning/GraphicalModels/02_LogLinearModels.R
+++ RLearning/GraphicalModels/02_LogLinearModels.R
@@ -69,3 +69,30 @@ m4 <- update(m3, list(dedge=~locc*a367))
 par(mfrow=c(1, 2))
 plot(m3, 'neato')
 plot(m4, 'neato')
+
+compare.models <- function(m1, m2) {
+  lrt <- m2$fitinfo$dev - m1$fitinfo$dev
+  df.diff <- m1$fitinfo$dimension[1] - m2$fitinfo$dimension[1]
+  c('lrt'=lrt, 'df'=df.diff)  
+}
+
+m3
+m4
+compare.models(m3, m4)
+testdelete(m3, edge=c('locc', 'a367')) # same
+
+# Test for condit indep
+cit <- ciTest_table(mildew, set=c('locc', 'a367', 'mp58', 'c365', 'p53a'))
+cit2 <- ciTest_table(mildew, 
+                     set=c('locc', 'a367', 'mp58', 'c365', 'p53a', 'la10'))
+cit$slice
+
+cit <- ciTest_table(
+  mildew, set=c('locc', 'a367', 'mp58', 'c365', 'p53a'), method='MC')
+ciTest_ordinal(dumping, c(2, 1, 3), 'jt', N=1000) # arg2 is df columns; N for MC
+ciTest_ordinal(dumping, c(3, 1, 2), 'kruskal', N=1000)
+
+
+
+# 4. Model Selection
+
