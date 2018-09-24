562ee099de263f357adc8b2c6903e2e6db94d8f8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-24 08:09:32 -0700

    adds cum haz model

--- RLearning/GAM/03GeneralizedLinearModels.R
+++ RLearning/GAM/03GeneralizedLinearModels.R
@@ -14,6 +14,7 @@
 rm(list = ls())
 setwd('~/Learning/R/RLearning/GAM')
 library(gamair)
+data(bone)
 data(stomata)
 
 
@@ -96,6 +97,64 @@ lines(new.t + 1980, exp(fv$fit - 1.96*fv$se.fit), col=2, lty=2)
 
 
 # 3.3 Cox proportional hazards modeling of survival data
+psurv <- function(surv, time='t', censor='d', event='z') {
+  # Create data.frame to fit Cox PH as Poisson model.
+  # surv[[censor]] should be 1 for event or 0 for censored
+  if (event %in% names(surv)) warning('event name clashes')
+  surv <- as.data.frame(surv)[order(surv[[time]]), ]
+  et <- unique(surv[[time]][surv[[censor]] == 1]) # unique times
+  es <- match(et, surv[[time]])                   # risk set starts
+  n <- nrow(surv)
+  t <- rep(et, 1 + n - es)                        # risk set times
+  st <- cbind(0, surv[unlist(apply(matrix(es), 1, function(x, n) x:n, n=n)), ])
+  st[st[[time]] == t & st[[censor]] != 0, 1] <- 1 # signal events
+  st[[time]] <- t                                 # event time -> risk set time]
+  names(st)[1] <- event
+  st
+}
+
+bone$id <- 1:nrow(bone)
+pb <- psurv(bone)
+pb$tf <- factor(pb$t)
+b <- glm(z ~ tf + trt - 1, poisson, pb) # -1 to ignore intercept
+
+# Compute resids
+chaz <- tapply(fitted(b), pb$id, sum) # by cum hazard
+mrsd <- bone$d - chaz                 # Martingale
+summary(b)  # Hazard est higher for 'auto' group by 0.7, but not signif
+drop1(b, test='Chisq') # same conclusion
+
+# Vis
+te <- sort(unique(bone$t[bone$d == 1])) # event times
+
+# pred surv func for 'allo'
+pd <- data.frame(tf=factor(te), trt=bone$trt[1]) # trt[1]
+fv <- predict(b, pd)
+H <- cumsum(exp(fv)) # cum hazard
+plot(stepfun(te, c(1, exp(-H))), 
+     do.points=F,
+     ylim=c(0, 1), 
+     xlim=c(0, 550), 
+     main='', 
+     ylab='S(t)', 
+     xlab='t (days)')
+# SE
+X <- model.matrix(~tf + trt - 1, pd)
+J <- apply(exp(fv) * X, 2, cumsum)
+se <- diag(J %*% vcov(b) %*% t(J))^0.5
+lines(stepfun(te, c(1, exp(-H + se))), do.points=F, col=2)
+lines(stepfun(te, c(1, exp(-H - se))), do.points=F, col=2)
+
+te2 <- sort(unique(bone$t[bone$d == 1])) # event times
+pd2 <- data.frame(tf=factor(te), trt=bone$trt[2]) 
+fv2 <- predict(b, pd2)
+H2 <- cumsum(exp(fv2)) # cum hazard
+lines(stepfun(te2, c(1, exp(-H2))), do.points=F, col='grey')
+X2 <- model.matrix(~tf + trt - 1, pd2)
+J2 <- apply(exp(fv2) * X2, 2, cumsum)
+se2 <- diag(J2 %*% vcov(b) %*% t(J2))^0.5
+lines(stepfun(te, c(1, exp(-H2 + se2))), do.points=F, col=4)
+lines(stepfun(te, c(1, exp(-H2 - se2))), do.points=F, col=4)
 
 
 	# 2.3.3 Log-linear models for categorical data
