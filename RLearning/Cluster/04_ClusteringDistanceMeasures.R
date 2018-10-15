f0589641ff17d5ae211505bf66f4b52893c69be4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-15 09:17:17 -0700

    4++

--- RLearning/Cluster/04_ClusteringDistanceMeasures.R
+++ RLearning/Cluster/04_ClusteringDistanceMeasures.R
@@ -11,3 +11,18 @@ df <- na.omit(df)
 df <- scale(df)
 
 head(df)
+
+
+# 4. Distance Matrix Computation
+# 4.1 Data preparation
+ss <- sample(1:50, 15)
+df <- USArrests[ss, ]
+df.scaled <- scale(df)
+
+
+# 4.3 Computing euclidean distance
+dist.eucl <- dist(df.scaled, method='euclidean')
+round(as.matrix(dist.eucl)[1:3, 1:3], 1)
+
+
+# 4.4 Computing correlation-based distances
