c34bb894b9248965d707cf9f9b54a7e4bc3a0dac
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-26 15:31:21 -0400

    -5, +6

--- RLearning/Cluster/06_kMeansClustering.R
+++ RLearning/Cluster/06_kMeansClustering.R
@@ -2,4 +2,13 @@
 rm(list=ls())
 setwd('~/Learning/R/RLearning/Cluster')
 
+#library(cluster)
+#library(factoextra)
+#data(USArrests)
+#data(flower)
 
+#df <- USArrests
+#df <- na.omit(df)
+#df <- scale(df)
+
+#head(df)
