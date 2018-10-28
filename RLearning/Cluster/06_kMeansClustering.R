79331adccb273ca2973695b4e87d7d71e1f7c255
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-28 19:16:53 -0400

    6++

--- RLearning/Cluster/06_kMeansClustering.R
+++ RLearning/Cluster/06_kMeansClustering.R
@@ -3,12 +3,19 @@ rm(list=ls())
 setwd('~/Learning/R/RLearning/Cluster')
 
 #library(cluster)
-#library(factoextra)
-#data(USArrests)
+library(factoextra)
+
+data(USArrests)
 #data(flower)
 
-#df <- USArrests
-#df <- na.omit(df)
-#df <- scale(df)
 
-#head(df)
+
+
+# 3 Computing k-Means Clusterin in R
+# 3.1 Data
+df <- scale(USArrests)
+head(df)
+
+
+# 3.3 Estimating the optimal number of clusters
+fviz_nbclust(df, kmeans, method='wss') + geom_vline(xintercept=4, lty=2)
