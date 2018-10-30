2c4d322b79b4629d8a518445a67a08f220287bcc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-29 22:47:22 -0400

    6++

--- RLearning/Cluster/06_kMeansClustering.R
+++ RLearning/Cluster/06_kMeansClustering.R
@@ -19,3 +19,9 @@ head(df)
 
 # 3.3 Estimating the optimal number of clusters
 fviz_nbclust(df, kmeans, method='wss') + geom_vline(xintercept=4, lty=2)
+
+
+# 3.4 Computing k-means clustering
+set.seed(456)
+km.res <- kmeans(df, 4, nstart=25)
+km.res
