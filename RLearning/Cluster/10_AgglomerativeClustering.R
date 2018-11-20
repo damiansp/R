91f97119250312099519f45a364999abf30c23d7
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-11-20 07:56:54 -0800

    10++

--- RLearning/Cluster/10_AgglomerativeClustering.R
+++ RLearning/Cluster/10_AgglomerativeClustering.R
@@ -6,3 +6,31 @@ lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
        unload=T)
 setwd('~/Learning/R/RLearning/Cluster')
 
+library(factoextra)
+data(USArrests)
+
+
+
+# 2. Steps to Agglomerative Hierarchical Clustering (AKA AgNes)
+df <- scale(USArrests)
+head(df)
+
+
+# 2.2 Similarity Measures
+# Dissimilarity matrix
+res.dist <- dist(df, method='euclidean')
+as.matrix(res.dist)[1:5, 1:5]
+
+
+# 2.3 Linkage
+res.hc <- hclust(d=res.dist, method='ward.D2')
+# methods: ward.D, single, complete, average, mcquitty, median, centroid
+# Ward minimizes within-cluster variance
+
+
+# 2.4 Dendrogram
+fviz_dend(res.hc, cex=0.5)
+
+
+
+# 3. Verify the Cluster Tree
