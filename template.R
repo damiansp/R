c8cec1cc46117e8de1b25262295c7f76c179bbea
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-11-21 08:03:56 -0800

    adds template and Cluster10++

---
+++ template.R
@@ -0,0 +1,7 @@
+#=========#=========#=========#=========#=========#=========#=========#=========
+rm(list=ls())
+lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
+       detach,
+       character.only=T,
+       unload=T)
+setwd('~/Learning/R/RLearning/...')
