8834d0674dd138ef6f549ba75f29a2074cdfa6fc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-30 07:49:19 -0800

    init 7

--- RLearning/Rbook/07Mathematics.R
+++ RLearning/Rbook/07Mathematics.R
@@ -4,4 +4,25 @@ lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
        detach,
        character.only=T,
        unload=T)
-setwd('~/Learning/R/RLearning/Cluster')
+setwd('~/Learning/R/RLearning/Rbook')
+
+
+
+# 1. Mathematical Functions
+
+
+# 1.1 Exponential Functions
+# log, exp
+
+
+# 1.2. Trig Functions
+# sin, cos, tan, ...
+
+
+# 1.3 Power Laws
+# x^y
+
+
+# 1.4 Polynomial Functions
+
+
