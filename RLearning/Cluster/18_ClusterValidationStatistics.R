a8de9dece09692f8b7a994c5fe8cd00213ed9596
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-02-12 08:21:39 -0800

    silhouette and validation

--- RLearning/Cluster/18_ClusterValidationStatistics.R
+++ RLearning/Cluster/18_ClusterValidationStatistics.R
@@ -35,3 +35,23 @@ fviz_dend(hc.res, show_labels=F, palette='jco', as.ggplot=T)
 
 # 3.4 Clustering validation
 # 3.4.1 Silhouette plot
+fviz_silhouette(km.res, palette='jco', ggtheme=theme_classic())
+sil.info <- km.res$silinfo
+names(sil.info)
+head(sil.info$widths)
+sil.info$clus.avg.widths
+sil.info$avg.width
+km.res$size
+sil <- km.res$silinfo$widths[, 1:3]
+
+# Objects with negative silhouette (likely misclassified)
+neg.sil.index <- which(sil[, 'sil_width'] < 0)
+sil[neg.sil.index, , drop=F]
+
+# 3.4.2 Validation Statistics
+km.stats <- cluster.stats(dist(df), km.res$cluster)
+km.stats
+km.stats$dunn
+
+
+# 3.5 External Clustering Validation
