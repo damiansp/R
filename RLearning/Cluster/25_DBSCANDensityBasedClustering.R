016b2aca9c1fd343cac3b1c9ab3c2e34cc15e810
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-26 07:36:04 -0700

    add to DBSCAN

--- RLearning/Cluster/25_DBSCANDensityBasedClustering.R
+++ RLearning/Cluster/25_DBSCANDensityBasedClustering.R
@@ -6,9 +6,11 @@ lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
        unload=T)
 setwd('~/Learning/R/RLearning/Cluster')
 
+library(dbscan)
 library(factoextra)
+library(fpc)
 
-data('multishapes')
+data('multishapes', package='factoextra')
 
 
 
@@ -24,4 +26,34 @@ fviz_cluster(km.res,
              geom='point', 
              palette='jco', 
              ggtheme=theme_classic())
-?fviz_cluster
+
+
+
+# 5. Computing DBSCAN
+db <- fpc::dbscan(df, eps=0.14, MinPts=5)
+fviz_cluster(db, 
+             data=df, 
+             stand=F, 
+             ellipse=T, 
+             ellipse.type='norm', 
+             geom='point', 
+             palette='jco')
+db
+db$cluster # oultiers are 0
+
+
+
+# 6. Optimizing Epsilon
+dbscan::kNNdistplot(df, k=5)
+abline(h=0.14, col=2)
+
+dbscan::kNNdistplot(df, k=8)
+abline(h=0.16, col=2)
+db <- fpc::dbscan(df, eps=0.16, MinPts=8)
+fviz_cluster(db, 
+             data=df, 
+             stand=F, 
+             ellipse=T, 
+             ellipse.type='norm', 
+             geom='point', 
+             palette='jco')
