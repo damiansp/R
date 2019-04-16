b6914fc8eadced1c83a80eed1467af5c4f88644a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-16 08:31:29 -0700

    7.1+ smooth spec

--- RLearning/GAM/07GAMSInPractice.R
+++ RLearning/GAM/07GAMSInPractice.R
@@ -6,4 +6,24 @@ lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
        unload=T)
 setwd('~/Learning/R/RLearning/GAM')
 
+library(MASS)
 library(mgcv)
+
+
+
+# 1. Specifying Smooths
+
+
+# 1.1 How smooth specification works
+# Set up a smoother
+head(mcycle)
+sm <- smoothCon(s(times, k=10), data=mcycle, knots=NULL)[[1]]
+# Use to fit a regression spline model
+beta <- coef(lm(mcycle$accel ~ sm$X - 1))
+# plot
+with(mcycle, plot(times, accel))
+# prediction times
+times <- seq(0, 60, length=200)
+# Get matrix mapping beta to spline prediction at <times>
+Xp <- PredictMat(sm, data.frame(times=times))
+lines(times, Xp %*% beta, col=2)
