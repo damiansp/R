b785f9bd12e6d2345abae2d64a8e84eece4f7d51
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-19 08:36:35 -0700

    init brain imaging example

--- RLearning/GAM/07GAMSInPractice.R
+++ RLearning/GAM/07GAMSInPractice.R
@@ -6,9 +6,11 @@ lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
        unload=T)
 setwd('~/Learning/R/RLearning/GAM')
 
+library(gamair)
 library(MASS)
 library(mgcv)
 
+data(brain)
 
 
 # 1. Specifying Smooths
@@ -27,3 +29,31 @@ times <- seq(0, 60, length=200)
 # Get matrix mapping beta to spline prediction at <times>
 Xp <- PredictMat(sm, data.frame(times=times))
 lines(times, Xp %*% beta, col=2)
+
+
+
+# 2. Brain Image Example
+
+
+# 2.1 Preliminary modeling
+head(brain)
+brain <- brain[brain$medFPQ > 5e-3, ] # exclude 2 outliers
+
+# Skew and fact that all values are positive in response suggest tranformation 
+# neccessary if using a Guassian model:
+m0 <- gam(medFPQ ~ s(Y, X, k=100), data=brain)
+gam.check(m0)
+
+# Clear heteroskedasticity... assume variance roughly proportional to µ^beta:
+e <- residuals(m0)
+fv <- fitted(m0)
+lm(log(e^2) ~ log(fv)) 
+# i.e., log(fv) = beta = 1.912 (var increases with the square of the mean)
+
+hist(brain$medFPQ^1.912)
+hist(brain$medFPQ^0.25)
+m1 <- gam(medFPQ^0.25 ~ s(Y, X, k=100), data=brain)
+gam.check(m1)
+
+m2 <- gam(medFPQ ~ s(Y, X, k=100), data=brain, family=Gamma(link=log))
+gam.check(m2)
